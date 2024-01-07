0dfd8e01a19a63a2099b2c11cc850fef17e08c8b
ewannemacher13 <ewannemacher13@gmail.com>
2024-01-07 11:01:56 -0500

    change <leader>pv to oil

--- lua/cfg/lazy/oil.lua
+++ lua/cfg/lazy/oil.lua
@@ -3,8 +3,9 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
         vim.keymap.set("n", "-", "<CMD>Oil<CR>")
+        vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>")
+
         require("oil").setup({
-            --columns = {},
             skip_confirm_for_simple_edits = true,
             view_options = {
                 show_hidden = true,
