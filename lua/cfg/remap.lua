1c8ad8d3c6807a45285989ed045510277285c311
ewannemacher13 <ewannemacher13@gmail.com>
2024-01-07 12:39:28 -0500

    remove conflicting list keymap

--- lua/cfg/remap.lua
+++ lua/cfg/remap.lua
@@ -35,5 +35,5 @@ vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lprev<CR>zz")
+--vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz")
+--vim.keymap.set("n", "<leader>k", "<cmd>lprev<CR>zz")
