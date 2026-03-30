71e315efb5e9e44145cafb38257d24217a21a955
Elliot Wannemacher <ewannemacher13@gmail.com>
2026-03-29 20:57:59 -0500

    select current buffer in harpoon window

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -15,7 +15,19 @@ return {
             harpoon:list():add()
         end)
         vim.keymap.set("n", "<leader>h", function()
+            local current = vim.api.nvim_buf_get_name(0)
             harpoon.ui:toggle_quick_menu(harpoon:list())
+            vim.schedule(function()
+                local cwd = vim.fn.getcwd() .. "/"
+                local rel_current = current:gsub("^" .. vim.pesc(cwd), "")
+                local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+                for i, line in ipairs(lines) do
+                    if line == rel_current or line == current then
+                        vim.api.nvim_win_set_cursor(0, { i, 0 })
+                        break
+                    end
+                end
+            end)
         end)
 
         vim.keymap.set("n", "<A-j>", function()
